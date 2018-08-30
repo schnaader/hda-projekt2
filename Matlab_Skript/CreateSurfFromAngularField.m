162ae352663f4785a5146fd228d89aae2eef3288
Christian Schneider <schnaader@gmx.de>
2018-08-30 13:23:41 +0200

    Matlab-Skript: Kommentare hinzugefügt

--- Matlab_Skript/CreateSurfFromAngularField.m
+++ Matlab_Skript/CreateSurfFromAngularField.m
@@ -1,11 +1,16 @@
 clear;
-filename = 'gebogen.csv';
+% CSV-Datei (wird vom Halcon-Skript erstellt)
+filename = 'Gebogen.csv';
+% Aus der CSV werden die Positionen der Referenzpunkte sowie deren
+% Verschiebung (jeweils in Pixeln) extrahiert
 [Row,Column, RowChanged, ColumnChanged] = textread(filename,'%*u %f %f %f %f %*f %*f','delimiter',';','headerlines',1);
 
-% % To create test data
- %count = 33;
- %noise = 10000;
- %for n = (1:count)    
+ % Dieser Block kann auskommentiert werden, um Testdaten zu erzeugen,
+ % statt Daten aus der CSV einzulesen
+ %
+ % count = 33;
+ % noise = 10000;
+ % for n = (1:count)    
  %    for m = (1:count)
  %        b = (n-1)*count+m;
  %        Column(b) = m;
@@ -15,7 +20,7 @@ filename = 'gebogen.csv';
  %        
  %        valid(b) = 1;
  %    end
- %end
+ % end
 
 RowChanged = RowChanged - Row;
 ColumnChanged = ColumnChanged - Column;
@@ -23,11 +28,13 @@ ColumnChanged = ColumnChanged - Column;
 Objektschnittweite = 710 / 1000; % in m
 Brennweite = 16 / 1000; % in m
 n = 1.51;
-d = 0.85/1000; % in m
+d = 0.85/1000; % Dicke der Oberfläche, in m
 Bildhoehe = 2748;
 Bildbreite = 3840;
-pixelGroesse = 1.67 * 10^-6; % in m pro pixel
+pixelGroesse = 1.67 * 10^-6; % in m, Seitenlänge eines Pixels
 
+% Die Referenzpunkte können in der CSV in beliebiger Reihenfolge vorliegen,
+% für den weiteren Ablauf hier ist es aber notwendig, sie zu sortieren
 count = numel(Row);
 index = Row * Bildbreite + Column;
 [SortedByIndex, Indices] = sort(index, 'ascend');
@@ -38,13 +45,15 @@ for i=1:count
     sortedColumn(i) = Column(Indices(i));
     sortedRowChanged(i) = RowChanged(Indices(i));
     sortedColumnChanged(i) = ColumnChanged(Indices(i));
-
 end
 Row = sortedRow;
 Column = sortedColumn;
 RowChanged = sortedRowChanged;
 ColumnChanged = sortedColumnChanged;
 
+% Wenn Referenzpunkte in Halcon nicht zugeordnet werden konnten, bekommen
+% sie als Position (0, 0) zugewiesen. Diese Punkte werden in der
+% valid-Liste als ungültige Punkte markiert.
 valid = ones(1,count);
 for i=1:count
     centeredX(i) = (Column(i) - Mittelpunkt(1)) * pixelGroesse / beta;
@@ -68,9 +77,7 @@ gammaX = gammaX * n / (d * (n-1)) - xAlpha;
 gammaY = ObjektDiffY./sqrt((ObjektDiffY./Objektschnittweite).^2 + 1);
 gammaY = gammaY * n / (d * (n-1)) - yAlpha;
 
-
-
-% Create matrices from vectors
+% Steigungsmatrizen erstellen
 sqrtCount = sqrt(count);
 Row_Mat = zeros(sqrtCount,sqrtCount);
 Column_Mat = zeros(sqrtCount,sqrtCount);
@@ -88,7 +95,7 @@ for n = 1:sqrtCount
     end
 end
 
-% Get the delta matrices
+% Matrizen erstellen, die den Abstand der Punkte untereinander beinhalten
 deltaRow_Mat = zeros(sqrtCount,sqrtCount);
 deltaColumn_Mat = zeros(sqrtCount,sqrtCount);
 for n = 1 : sqrtCount
@@ -98,39 +105,46 @@ for n = 1 : sqrtCount
     end
 end
 
-%Do the interpolation here
+% Berechnung und Interpolation der Oberfläche
+% Startpunkt der Rekonstruktion ist die linke obere Ecke. Entlang der Ränder
+% ist die Rekonstruktion dann eindeutig, zu allen anderen Punkten führen
+% dann zwei Wege (von links sowie von oben), von diesen beiden wird der
+% Mittelwert verwendet.
 Height_Mat = zeros(sqrtCount,sqrtCount);
 for n = (1:sqrtCount)
     for m = (1:sqrtCount)        
         if(n==1 && m==1)   
             Height_Mat(n,m) = 0;
         elseif(n==1)
-            % only interpolate in x direction
+            % Rand in X-Richtung
             Height_Mat(n,m) = Height_Mat(n,m-1) + deltaColumn_Mat(n,m) *  xGradient_Mat(m,n); 
         elseif(m==1)
-            %only interpolate in y direction
+            % Rand in Y-Richtung
             Height_Mat(n,m) = Height_Mat(n-1,m) + deltaRow_Mat(n,m) *  yGradient_Mat(m,n); 
         else
-            %average from x and y direction
-            xPart = Height_Mat(n, m-1) + deltaColumn_Mat(n,m) *  xGradient_Mat(m,n);
-            yPart = Height_Mat(n-1, m) + deltaRow_Mat(n,m) *  yGradient_Mat(m,n);
+            % Durchschnitt der aus der X- sowie Y-Richtung kommenden Höhen
+            xPart = Height_Mat(n, m-1) + deltaColumn_Mat(n,m) * xGradient_Mat(m,n);
+            yPart = Height_Mat(n-1, m) + deltaRow_Mat(n,m) * yGradient_Mat(m,n);
             Height_Mat(n,m) = mean([xPart, yPart]);
         end
     end
 end
 
-%[X,Y] = meshgrid(1:sqrtCount,1:sqrtCount);
+% Plots erstellen
 
-%Plot stuff
-%surf(Row_Mat,Column_Mat,Height_Mat)
+% Plot der Referenzpunkte ohne Interpolation
 figure (1)
 plot3(Row_Mat,Column_Mat,Height_Mat,'.')
+
+% Rekonstruktion der Oberfläche mit maßstabsgetreuen Achsen
 figure (2)
-%hold on
-surf(Row_Mat,Column_Mat,Height_Mat)
-xlabel('x');
-ylabel('y');
+hold on
+surf(Row_Mat*1000,Column_Mat*1000,Height_Mat*1000)
+xlabel('x / mm');
+ylabel('y / mm');
+zlabel('z / mm');
+axis equal
 
- figure(3)
- quiver(Column,Row,ColumnChanged,RowChanged)
-
+% Plot der Verschiebungsvektoren der Referenzpunkte
+% figure(3)
+% quiver(Column,Row,ColumnChanged,RowChanged)
